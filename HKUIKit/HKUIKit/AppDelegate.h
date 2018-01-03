91f35cd00979d99784d79f2be0d6cb1c057c3ed6
Kun Huang <kunhuang@KundeMacBook-Air.local>
2018-01-03 17:12:52 +0800

    123

---
+++ HKUIKit/HKUIKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HKUIKit
+//
+//  Created by Kun Huang on 2018/1/3.
+//  Copyright © 2018年 Kun Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HKUIKit/HKUIKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HKUIKit
+//
+//  Created by Kun Huang on 2018/1/3.
+//  Copyright © 2018年 Kun Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
